1ba4d24daf7c4ef70cb1f776a85066d1df9b4db9
Ravi Thangiah Soundarapandian <Ravi.Shankar@kohls.com>
2018-02-24 23:16:25 -0600

    Simplified implicits

--- app/com/cart/sale/util/RedisHelper.scala
+++ app/com/cart/sale/util/RedisHelper.scala
@@ -19,24 +19,9 @@ class RedisHelper @Inject()(aSys: ActorSystem) {
   var redis: SentinelMonitoredRedisClient = _
 
   implicit val akkaSystem = aSys
-
-  implicit val serString = new ByteStringSerializer[String] {
-    override def serialize(data: String): ByteString = {
-      ByteString(data)
-    }
-  }
-
-  implicit val serAny = new ByteStringSerializer[Any] {
-    override def serialize(data: Any): ByteString = {
-      ByteString(Json.toJson(data).asText())
-    }
-  }
-
-  implicit val deSerAny = new ByteStringDeserializer[Cart] {
-    override def deserialize(bs: ByteString): Cart = {
-      Json.fromJson[Cart](Json.parse(bs.utf8String), classOf[Cart])
-    }
-  }
+  implicit val serString: ByteStringSerializer[String] = (data: String) => ByteString(data)
+  implicit val serAny: ByteStringSerializer[Any] = (data: Any) => ByteString(Json.toJson(data).asText())
+  implicit val deSerAny: ByteStringDeserializer[Cart] = (bs: ByteString) => Json.fromJson[Cart](Json.parse(bs.utf8String), classOf[Cart])
 
   def init {
     redis = new SentinelMonitoredRedisClient(Seq(("localhost", 26179), ("localhost", 26381)), "redis-cluster")
